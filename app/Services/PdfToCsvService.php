a4b1736c400c13c6cad76793cfcf1a8a30275647
André Kotake <andre.kotake@itixti.com.br>
2021-12-08 11:57:30 -0300

    Adicionado leitura de strings do arquivo PDF

--- app/Services/PdfToCsvService.php
+++ app/Services/PdfToCsvService.php
@@ -2,17 +2,54 @@
 
 namespace App\Services;
 
-use Illuminate\Support\Facades\Storage;
-use Spatie\PdfToText\Pdf;
+use Smalot\PdfParser\Parser;
 
 class PdfToCsvService
 {
-
     function convert()
     {
-        $file = storage_path('app/files') . '/LeituraPDF.pdf';
-        $text = (new Pdf())
-            ->setPdf($file)
-            ->text();
+        $filePath = storage_path('app/files') . '/LeituraPDF.pdf';
+        $pdfParser = new Parser();
+        $pdf = $pdfParser->parseFile($filePath)->getText();
+
+        $columns = array(
+            'Registro ANS',
+            'Nome da Operadora'
+        );
+
+        $callback = function () use ($pdf, $columns) {
+            $file = fopen(storage_path('app/files/pdf_to_csv.csv'), 'wa+');
+            fputcsv($file, $columns);
+            fputcsv(
+                $file,
+                array(
+                    $this->formatRegistroAns($pdf),
+                    $this->formatNomeOperadora($pdf),
+                )
+            );
+
+            fclose($file);
+        };
+
+        $callback();
+        return storage_path('app/files/pdf_to_csv.csv');
+    }
+
+    private function formatRegistroAns($pdf)
+    {
+        $indexInicial = strpos($pdf, 'Registro ANS') + 1;
+        $indexFinal = strpos($pdf, 'Nome da Operadora');
+        $registroAnsValor = substr($pdf, $indexInicial, $indexFinal - $indexInicial - 3); // -3 para remover os caracteres " - "
+
+        return substr($registroAnsValor, strlen('Registro ANS'));
+    }
+
+    private function formatNomeOperadora($pdf)
+    {
+        $indexInicial = strpos($pdf, 'Nome da Operadora') + 1;
+        $indexFinal = strpos($pdf, 'CNPJ da Operadora');
+        $registroAnsValor = substr($pdf, $indexInicial, $indexFinal - $indexInicial - 3); // -3 para remover os caracteres " - "
+
+        return substr($registroAnsValor, strlen('Nome da Operadora'));
     }
 }
